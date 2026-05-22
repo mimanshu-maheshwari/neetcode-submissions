c0ed5e70d65f6ed26c201d70abcef9e1ea6465e0
Mimanshu Maheshwari <35875722+mimanshu-maheshwari@users.noreply.github.com>
2026-05-22 18:06:52 +0530

    Add: reorganize-string - submission-0

---
+++ Data Structures & Algorithms/reorganize-string/submission-0.java
@@ -0,0 +1,38 @@
+class Solution {
+    public String reorganizeString(String s) {
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> b[1] - a[1]);
+        StringBuilder result = new StringBuilder();
+        Map<Character, Integer> freq = new HashMap<>();
+        for (char c: s.toCharArray()) {
+            freq.merge(c, 1, Integer::sum);
+        }
+        for (Map.Entry<Character, Integer> entry: freq.entrySet()) {
+            pq.offer(new int[]{entry.getKey(), entry.getValue()});
+        }
+        while (!pq.isEmpty()) {
+            int[] curr = pq.poll();
+            char c = (char) curr[0];
+            int count = curr[1];
+            if (result.length() > 0 && result.charAt(result.length() - 1) == c) {
+                if (pq.isEmpty()){
+                    return "";
+                }
+                int[] second = pq.poll();
+                char sc = (char) second[0];
+                result.append(sc);
+                second[1]--;
+                if (second[1] > 0){
+                    pq.offer(second);
+                }
+                pq.offer(curr);
+            }else {
+                result.append(c);
+                curr[1]--;
+                if (curr[1] > 0){ 
+                    pq.offer(curr);
+                }
+            }
+        }
+        return result.toString();
+    }
+}
