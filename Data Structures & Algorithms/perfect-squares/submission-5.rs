aecfd883883eb0681a494c891e79b51ca25745dc
Mimanshu Maheshwari <35875722+mimanshu-maheshwari@users.noreply.github.com>
2026-06-16 14:48:03 +0530

    Add: perfect-squares - submission-5

---
+++ Data Structures & Algorithms/perfect-squares/submission-5.rs
@@ -0,0 +1,15 @@
+impl Solution {
+    pub fn num_squares(n: i32) -> i32 {
+        let n = n as usize;
+        let mut dp = vec![n as i32; n + 1];
+        dp[0] = 0;
+        for target in 1..=n {
+            let mut s = 1;
+            while s * s <= target {
+                dp[target] = dp[target].min(1 + dp[target - (s * s)]);
+                s += 1;
+            }
+        }
+        dp[n]
+    }
+}
