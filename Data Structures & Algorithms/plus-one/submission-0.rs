7b6733c989d26d9c5365296175af979a581318d2
Mimanshu Maheshwari <35875722+mimanshu-maheshwari@users.noreply.github.com>
2026-06-18 15:17:26 +0530

    Add: plus-one - submission-0

---
+++ Data Structures & Algorithms/plus-one/submission-0.rs
@@ -0,0 +1,14 @@
+impl Solution {
+    pub fn plus_one(mut digits: Vec<i32>) -> Vec<i32> {
+        let mut carry = 1;
+        for i in (0..digits.len()).rev() {
+            let val = digits[i] + carry;
+            digits[i] = val % 10;
+            carry = val / 10;
+        }
+        if carry > 0 {
+            digits.insert(0, carry);
+        }
+        digits
+    }
+}
