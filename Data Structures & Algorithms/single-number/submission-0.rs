78d958d5ac468b4f04671a9826059929b776db97
Mimanshu Maheshwari <35875722+mimanshu-maheshwari@users.noreply.github.com>
2026-06-20 14:22:19 +0530

    Add: single-number - submission-0

---
+++ Data Structures & Algorithms/single-number/submission-0.rs
@@ -0,0 +1,10 @@
+impl Solution {
+    pub fn single_number(nums: Vec<i32>) -> i32 {
+        let mut xor = 0;
+        for num in nums {
+            xor ^= num;
+        }
+        xor ^= 0;
+        xor
+    }
+}
