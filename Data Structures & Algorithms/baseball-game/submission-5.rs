b04034ac16ea8885f35eca6601c47294e14610a7
Mimanshu Maheshwari <35875722+mimanshu-maheshwari@users.noreply.github.com>
2026-06-24 21:17:58 +0530

    Add: baseball-game - submission-5

---
+++ Data Structures & Algorithms/baseball-game/submission-5.rs
@@ -0,0 +1,15 @@
+impl Solution {
+    pub fn cal_points(operations: Vec<String>) -> i32 {
+        let mut data:Vec<i32> = Vec::new();
+        for op in operations {
+            let op = op.as_str();
+            match op {
+                "+" => data.push(data[data.len() - 1] + data[data.len() - 2]),
+                "D" => data.push(data[data.len() - 1] * 2),
+                "C" => {data.pop(); },
+                n   => data.push(n.parse().expect("Invalid number!!")),
+            }
+        }
+        data.into_iter().sum()
+    }
+}
