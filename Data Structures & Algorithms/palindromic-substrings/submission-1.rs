43809842427b156ac8a5a0551671829e7a9adb74
Mimanshu Maheshwari <35875722+mimanshu-maheshwari@users.noreply.github.com>
2026-07-03 11:25:17 +0530

    Add: palindromic-substrings - submission-1

---
+++ Data Structures & Algorithms/palindromic-substrings/submission-1.rs
@@ -0,0 +1,30 @@
+impl Solution {
+    pub fn count_substrings(s: String) -> i32 {
+        let mut count: i32 = 0;
+        let s: Vec<char> = s.chars().collect();
+        for i in 0..s.len() {
+            count += Self::is_palindrome(&s, i, i);
+            if (i > 0) {
+                count += Self::is_palindrome(&s, i - 1, i);
+            }
+        }
+        count
+    }
+
+    #[inline(always)]
+    fn is_palindrome(s: &[char], mut start: usize, mut end: usize) -> i32 {
+        let mut count = 0;
+        while start >= 0 && end < s.len() {
+            if s[start] != s[end] {
+                break;
+            }
+            count += 1;
+            if (start == 0 || end == s.len()){
+                break;
+            }
+            start -= 1;
+            end   += 1;
+        }
+        count
+    }
+}
