f3995a151b508ce7bf684683e658c5df6284767f
Mimanshu Maheshwari <35875722+mimanshu-maheshwari@users.noreply.github.com>
2026-07-14 08:18:06 +0530

    Add: minimum-array-end - submission-2

---
+++ Data Structures & Algorithms/minimum-array-end/submission-2.rs
@@ -0,0 +1,17 @@
+impl Solution {
+    pub fn min_end(n: i32, x: i32) -> i64 {
+        let mut res = x as i64;
+        let mut m = (n - 1) as i64;  // counter value for the last element
+        let mut bit = 1i64;
+        while m > 0 {
+            if bit & x as i64 == 0 {  // this is a free bit position
+                if m & 1 == 1 {        // corresponding bit of counter is 1
+                    res |= bit;
+                }
+                m >>= 1;               // consume one bit of the counter
+            }
+            bit <<= 1;                 // advance to next position
+        }
+        res
+    }
+}
