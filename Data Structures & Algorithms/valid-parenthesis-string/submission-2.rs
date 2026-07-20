321d5d029fb30f22594a68bcfaab064ab9d2a545
Mimanshu Maheshwari <35875722+mimanshu-maheshwari@users.noreply.github.com>
2026-07-20 12:11:19 +0530

    Add: valid-parenthesis-string - submission-2

---
+++ Data Structures & Algorithms/valid-parenthesis-string/submission-2.rs
@@ -0,0 +1,17 @@
+impl Solution {
+    pub fn check_valid_string(s: String) -> bool {
+        let (mut low, mut high) = (0i32, 0i32);
+        
+        for c in s.chars() {
+            match c {
+                '(' => {low += 1; high += 1;},
+                ')' => {low -= 1; high -= 1;},
+                _   => {low -= 1; high += 1;},
+            }
+            if high < 0 { return false; }
+            low = low.max(0);
+        }
+        low == 0
+
+    }
+}
